dd822e6b672ca2ec180716be09b52b02f8366fba
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-01-14 15:22:58 +0200

    Initialize the project

---
+++ scripts/temp.ts
@@ -0,0 +1,3 @@
+import {temp} from '../src/index';
+
+console.log('This is a temporary script 2, 3 ->', temp(2, 3));
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export function temp(a: number, b: number) {
+  return 2 * a + b;
+}
